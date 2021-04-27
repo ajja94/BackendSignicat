2ec49b2b5d273c036b2edc7a633cef04aae3e4b6
ajja94 <anja@getacademy.no>
2021-04-27 22:20:31 +0200

    update

--- SignicatExpress/SignicatOppgave/Program.cs
+++ SignicatExpress/SignicatOppgave/Program.cs
@@ -12,6 +12,7 @@ namespace SignicatOppgave
     {
         static async Task Main(string[] args)
         {
+            //laget egen classe med cliens 
             var clientInfo = new Clients();
 
             var client = new IdentificationV2Service(clientInfo.GetClientId(), clientInfo.GetClientSecret(),
@@ -21,6 +22,7 @@ namespace SignicatOppgave
                 }) ;
 
             var session = await client.CreateSessionAsync(new IdSessionCreateOptions()
+            //valg av hvilken tyåe inn logging du ønsker, bankId/sms osv
             {
                 AllowedProviders = new List<IdProviderType>()
             {
@@ -29,13 +31,15 @@ namespace SignicatOppgave
                  IdProviderType.SmsOtp
                 
             },
-
+            //redirect Url'er med success/error og abort 
+            // success er satt til en hjemmelaget html front end med kun success status på V-1 
             RedirectSettings = new RedirectSettings()
             {
                 ErrorUrl = "https://www.signicat.com#error/",
                 AbortUrl = "https://www.signicat.com#abort/",
                 SuccessUrl = "https://ajja94.github.io/SignicatFrontend/ClientApp/src/app/home/home.component.html"
             },
+            //valg av språk og 
             ExternalReference = Guid.NewGuid().ToString("n"),
             Flow = IdSessionFlow.Redirect,
             Language = Language.No,
@@ -44,6 +48,7 @@ namespace SignicatOppgave
                 Include.Nin,
                 Include.Name,
             },
+            //styiling
             Ui = new UiSettings()
             {
                 ColorTheme = ColorTheme.Default,
